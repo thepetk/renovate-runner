12ed2efbdf9d690aff1053b6bbd4384334823b61
Liora Milbaum <lmilbaum@redhat.com>
2023-07-06 09:59:25 +0300

    gitlab-configuration repo

--- config.js
+++ config.js
@@ -6,10 +6,8 @@ module.exports = {
         'platform-engineering-org/helper',
         'platform-engineering-org/gitlab-runners',
         'platform-engineering-org/platform-engineering-org.github.io',
+        'platform-engineering-org/gitlab-configuration',
         'arcalot/arcaflow-plugin-aws-ec2-control',
         'crc-org/crc-cloud'
-    ],
-    "vulnerabilityAlerts": {
-      "labels": ["security"]
-    }
+    ]
 };
