f51e24ed97e86470a9be77f0c2e4a59ad3dc7fab
Liora Milbaum <lmilbaum@redhat.com>
2023-07-13 13:09:57 +0300

    Onboaring arcaflow-plugin-example and arcaflow-plugin-template-python

--- config.js
+++ config.js
@@ -1,5 +1,15 @@
 module.exports = {
     platform: 'github',
+    onboardingConfig: {
+        "extends": [
+            "config:base",
+            ":enablePreCommit",
+            "group:allNonMajor",
+            "regexManagers:dockerfileVersions",
+            "schedule:weekly",
+            "docker:pinDigests"
+        ]
+    },
     repositories: [
         'platform-engineering-org/renovate-runner',
         'platform-engineering-org/bootstrap',
@@ -8,6 +18,8 @@ module.exports = {
         'platform-engineering-org/platform-engineering-org.github.io',
         'platform-engineering-org/gitlab-configuration',
         'arcalot/arcaflow-plugin-aws-ec2-control',
+        'arcalot/arcaflow-plugin-example',
+        'arcalot/arcaflow-plugin-template-python',
         'crc-org/crc-cloud'
     ]
 };
