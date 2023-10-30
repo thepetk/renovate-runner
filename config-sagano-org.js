4da442727539ac75e2c2c37435e492549350c923
Liora Milbaum <lmilbaum@redhat.com>
2023-10-30 06:03:35 +0200

    Create config-sagano-org.js

---
+++ config-sagano-org.js
@@ -0,0 +1,19 @@
+module.exports = {
+  platform: "github",
+  onboardingConfig: {
+    "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+    "extends": [
+      "local>sagano-org/.github"
+    ]
+  },
+  hostRules: [
+    {
+      "hostType": "docker",
+      "matchHost": "docker.io",
+      "username": process.env.DOCKER_HUB_USERNAME,
+      "password": process.env.DOCKER_HUB_TOKEN
+    }
+  ],
+  autodiscover: true,
+  autodiscoverFilter: ["sagano-org/*"]
+};
