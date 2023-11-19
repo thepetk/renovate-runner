a675c951db89ac2ad7444f537e1157631397bb01
Liora Milbaum <lmilbaum@redhat.com>
2023-11-19 15:55:35 +0200

    Update config-other.js

--- config-other.js
+++ config-other.js
@@ -19,7 +19,8 @@ module.exports = {
     "crc-org/crc-cloud",
     "devfile/registry",
     "thepetk/registry",
-    "ralphbean/dnf-plugin-lockfile"
+    "ralphbean/dnf-plugin-lockfile",
+    "opdev/fips-assessments"
   ],
   constraints: {
     go: "1.19"
